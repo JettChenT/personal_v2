1924739b1e741a0d9bfb7f45c6c7d824d934bd25
Jett Chen <jettchen12345@gmail.com>
2024-09-24 18:39:11 +0800

    better stream

--- app/stream/page.tsx
+++ app/stream/page.tsx
@@ -5,9 +5,12 @@ const prisma = new PrismaClient();
 export default async function StreamPage() {
   const posts = await prisma.streamPost.findMany();
   return (
-    <div className="container mx-auto mt-10">
+    <div className="container mx-auto mt-10 lg:w-1/2 xl:w-1/3">
       {posts.map((post) => (
-        <div key={post.id}>{post.title}</div>
+        <div key={post.id} className="p-2 mb-4">
+          <h2 className="text-2xl font-bold mb-2">{post.title}</h2>
+          <p className="text-gray-700">{post.content}</p>
+        </div>
       ))}
     </div>
   );
